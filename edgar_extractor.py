5d533ad5e955c2f92e529c4ba71400aa5adf705e
joshgryzen <58818027+joshgryzen@users.noreply.github.com>
2024-06-25 08:00:42 -0500

    Initial Commit with updated code

---
+++ edgar_extractor.py
@@ -0,0 +1,74 @@
+from sec_edgar_downloader import Downloader
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "-c", "--companies", required=True, nargs="+", help="A list of company tickers"
+)
+
+parser.add_argument(
+    "-f",
+    "--form",
+    required=True,
+    type=str,
+    help="The requested form type, ie: 10-K, 10-Q",
+)
+
+parser.add_argument(
+    "-s",
+    "--start",
+    required=False,
+    type=int,
+    default=2014,
+    help="The starting year as an integer",
+)
+
+parser.add_argument(
+    "-e",
+    "--end",
+    required=False,
+    type=int,
+    default=2024,
+    help="The ending year as an integer",
+)
+
+args = vars(parser.parse_args())
+
+companies = args["companies"]
+form = args["form"]
+start = args["start"]
+end = args["end"]
+
+# List of company tickers
+# companies = [
+#     "CLH",
+#     "DY",
+#     "EME",
+#     "FIX",
+#     "FSS",
+#     "HUBB",
+#     "MTZ",
+#     "PRIM",
+#     "RBC",
+#     "ROAD",
+#     "SPXC",
+#     "STRL",
+# ]
+
+for company in companies:
+
+    dl = Downloader(
+        "Gryzen Financial Group, LLC",
+        "gryzencoaching@gmail.com",
+    )
+
+    # Set to 10 years
+    for year in range(start, end):
+        dl.get(
+            form,
+            company,
+            after=f"{year}-01-01",
+            before=f"{year+1}-01-01",
+            # download_details=False,
+        )
